97707e96d1d31547c12720427de4e610979cf29c
Dan Wood <daniel4wood@icloud.com>
2020-07-14 11:21:42 +1000

    Add tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,26 @@
 module.exports = {
   purge: ["./src/**/*.js"],
   theme: {
-    linearGradients: theme => ({
-      colors: theme("colors")
+    linearGradients: (theme) => ({
+      colors: theme("colors"),
     }),
-    radialGradients: theme => ({
-      colors: theme("colors")
+    radialGradients: (theme) => ({
+      colors: theme("colors"),
     }),
-    conicGradients: theme => ({
-      colors: theme("colors")
+    conicGradients: (theme) => ({
+      colors: theme("colors"),
     }),
     extend: {
       fontFamily: {
-        montserrat: ["Montserrat", "sans-serif"]
-      }
-    }
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   variants: {
-    textColor: ["responsive", "hover", "focus", "group-hover"]
+    textColor: ["responsive", "hover", "focus", "group-hover"],
   },
-  plugins: [require("tailwindcss-gradients")()]
+  plugins: [
+    require("tailwindcss-gradients")(),
+    require("@tailwindcss/typography"),
+  ],
 };
